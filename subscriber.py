3a156ee829aa6eb180b6d2ea758d03d4c5971cea
Danielafe7GitHub <daniela.milon@ucsp.edu.pe>
2018-07-20 11:08:55 -0500

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -8,55 +8,100 @@ import zbar
 import numpy as np
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 
-d1 = 0
-d2 = 0
+#Variables Globales
+d1 = None
+d2 = None
+texto = ""
+texto2 = ""
+t_x = 0
+t_y = 0
+vel_lin = 0.4
+vel_ang = 0.5
+mov = Twist() 
+
 def callback(data):
     global d1
     global d2
-    print("X: ",data.x," Y: ",data.y)
+    #print("X: ",data.x," Y: ",data.y)
     d1 = int(data.x)
     d2 = int(data.y) 
+    if d1 == 0 and d2 == 0:
+        d1 = None
+        d2 = None
 
-def talker(data):
+def talker(data,param):
+    global texto
+    global texto2
+    global t_x
+    global t_y
     image = None
     bridge = CvBridge()
     try:
         image = bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
         print(e)
-    output = image.copy()   
-
-    # Centro QR Code (d1,d2)
-    cv2.circle(output,(d1,d2), 5, (0,0,255), -1)    
 
     # Centro Video Frame (c1,c2)
-    c1 = 320
-    c2 = 180
-    cv2.circle(output,(c1,c2), 20, (0,90,255), -1)
-    cv2.line(output,(c1,c2),(d1,d2),(0,0,255),5)
+    height, width, channels = image.shape
+    c1 = width / 2
+    c2 = height / 2
+    cv2.circle(image,(c1,c2), 20, (0,90,255), -1)
+    
 
-    #print("Centro Frame :",c1, " ",c2)
-    #print("Centro QR code :",d1, " ",d2)
-    # Movimientos
-    if c1 < d1:
-        print("Muevete a la derecha !!!")
-    if c1 > d1:
-        print("Muevete a la izquierda !!!")
-    if c2 > d2:
-        print("Muevete hacia arriba !!!")
-    if c2 < d2:
-        print("Muevete hacia abajo !!!")
+    if d1 != None and d2 != None:
+        # Centro QR Code (d1,d2)
+        cv2.circle(image,(d1,d2), 5, (0,0,255), -1) 
+        cv2.line(image,(c1,c2),(d1,d2),(0,0,255),5)   
+        # Movimientos
+        if c1 < d1:
+            mov.linear.x = vel_lin  * (-1)
+            t_x = 500
+            texto = "Derecha"
+            #print("Muevete a la derecha !!!")
+        #if c1 > d1:
+        else:
+            mov.linear.x = vel_lin * (1)
+            t_x = 0
+            texto = "Izquierda"
+            #print("Muevete a la izquierda !!!")
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(image,texto,(t_x,180), font, 1,(255,255,255),2)
+        param.publish(mov)
+        if c2 > d2:
+            mov.linear.y = vel_lin  * (-1)
+            t_y = 30
+            texto2 = "Arriba"
+            #print("Muevete hacia arriba !!!")
+        #if c2 < d2:
+        else:
+            mov.linear.y = vel_lin * (1)
+            t_y = 320
+            texto2 = "Abajo"
+            #print("Muevete hacia abajo !!!")
+
+        param.publish(mov)
+        mov.linear.x = 0 
+        mov.linear.y = 0 
+        mov.linear.z = 0 
+        param.publish(mov)
+        
+        cv2.putText(image,texto2,(320,t_y), font, 1,(255,255,255),2)
 
 
-    cv2.imshow("Image", output)
+    
+    
+    cv2.imshow("Image", image)
     cv2.waitKey(3)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
     rospy.Subscriber("preprocesamiento", vector21, callback)
-    rospy.Subscriber("/ardrone/front/image_raw", Image, talker)
+    rospy.Subscriber("/ardrone/front/image_raw", Image, talker, pub)
 
     rospy.spin()
 
